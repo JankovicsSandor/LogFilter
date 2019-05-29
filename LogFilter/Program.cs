72e1d68094901cb871eead1c0712de1e1764b224
janksand <sanyika717022@gmail.com>
2019-05-29 19:33:43 +0200

    Add project files.

---
+++ LogFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
